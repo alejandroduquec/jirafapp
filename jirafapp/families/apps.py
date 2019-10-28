11e2adb602458800d32770138e0d704d9ef7e544
Alejandro Duque <ducode@outlook.com>
2019-10-28 02:29:14 -0500

    initial family modeling

---
+++ jirafapp/families/apps.py
@@ -0,0 +1,10 @@
+"""Families app."""
+
+from django.apps import AppConfig
+
+
+class FamiliesAppConfig(AppConfig):
+    """Families app config."""
+
+    name = 'jirafapp.families'
+    verbose_name = 'Families'
